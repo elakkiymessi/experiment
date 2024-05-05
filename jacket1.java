dc1202b1101c2c9f491e4c6c895ac4ad267f20cb
elakki <elakkiyanm.22aim@kongu.edu>
2024-05-05 09:46:44 +0530

    message

---
+++ jacket1.java
@@ -0,0 +1,17 @@
+class TestDTM{
+	String name;
+	int rollno;
+	
+static String clgname;
+static int counter=0;
+
+public student(String name)
+{
+	this.name=name;
+	this.rolllno=Setrollno();
+}
+static int Setrollno()
+{
+	counter
+}
+}212
